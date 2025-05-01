e5aa439bdb38913ff48d8af949fed297f1495637
Diogo <diogo.alex.00@gmail.com>
2025-05-01 06:06:00 +0100

    fg

--- client/src/components/AboutSection.tsx
+++ client/src/components/AboutSection.tsx
@@ -16,7 +16,7 @@ const teamMembers: TeamMember[] = [
       "Beatriz Abrantes tem mais de 30 anos de experiência no setor financeiro internacional. É formada em Economia pela Universidade de Lisboa e doutorada em Finanças pela London School of Economics.",
       "Ocupou cargos de direção em diversas instituições bancárias na Europa e nos Estados Unidos, previamente à fundação de Glosadores e Associados em 2005. É reconhecida pela sua visão estratégica e compromisso com a excelência."
     ],
-    image: "",
+    image: "/images/og.jpg",
     alt: "Beatriz Abrantes"
   },
   {
