76b0325b187404863b722f86cbf2a633ce031ce7
Diogo <diogo.alex.00@gmail.com>
2025-05-05 03:03:56 +0100

    test

--- client/src/components/Conclusao.tsx
+++ client/src/components/Conclusao.tsx
@@ -1,6 +1,6 @@
 import { ShieldCheck, Gavel, BookText, UserCheck, AlertTriangle, Scale, Lock, Briefcase, Banknote } from 'lucide-react';
 import { useEffect } from "react";
-import { Link } from 'wouter';
+import { Link } from "wouter";
 
 const Conclusao = () => {
   useEffect(() => {
--- client/src/components/Integracao.tsx
+++ client/src/components/Integracao.tsx
@@ -1,6 +1,6 @@
 import { Gavel, Scale, Banknote } from 'lucide-react';
 import { useEffect } from "react";
-import { Link } from 'wouter';
+import { Link } from "wouter";
 
 
 const Integracao = () => {
