ed1001c507a7318edfbf5c869463c5ec7ff7c53d
Diogo <diogo.alex.00@gmail.com>
2025-05-05 02:38:01 +0100

    test

--- client/src/components/AboutSection.tsx
+++ client/src/components/AboutSection.tsx
@@ -22,7 +22,7 @@ const teamMembers: TeamMember[] = [
       "Beatriz Abrantes tem mais de 30 anos de experiência no setor financeiro internacional. É formada em Economia pela Universidade de Lisboa e doutorada em Finanças pela London School of Economics.",
       "Ocupou cargos de direção em diversas instituições bancárias na Europa e nos Estados Unidos, previamente à fundação de Glosadores e Associados em 2005. É reconhecida pela sua visão estratégica e compromisso com a excelência."
     ],
-    image: "",
+    image: "/images/babrantes.jpeg",
     alt: "Beatriz Abrantes"
   },
   {
@@ -74,7 +74,7 @@ const teamMembers: TeamMember[] = [
       "Rodrigo Chaparro lidera a nossa transformação digital e iniciativas de inovação bancária. É formado em Engenharia Informática pelo Instituto Superior Técnico e com MBA pelo MIT.",
       "A sua experiência em fintech e banca digital tem sido essencial para desenvolver soluções tecnológicas seguras que mantêm os Glosadores e Associados na vanguarda do sector bancário, combinando tradição com inovação responsável."
     ],
-    image: "",
+    image: "images/rchaparro.jpeg",
     alt: "Rodrigo Chaparro"
   }
 ];
